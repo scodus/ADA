028c9d519d637b9288635d3f84c5fb8abb42b0aa
brraut <bishowraut@gmail.com>
2023-03-01 02:10:36 +0545

    upload

---
+++ pageScript.js
@@ -0,0 +1,45 @@
+var link = document.createElement("link");
+
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute("href", "/css/widget.css");
+
+document.getElementsByTagName("head")[0].appendChild(link);
+
+setTimeout(function () {
+  function AdaBundleGoogleTranslateElementInit(lang) {
+    lang = lang || "fr";
+    new google.translate.TranslateElement(
+      { pageLanguage: "en" },
+      "adabundle-translate-element"
+    );
+  }
+
+  AdaBundleGoogleTranslateElementInit();
+}, 9000);
+
+var script = document.createElement("script");
+
+script.setAttribute("type", "text/javascript");
+script.setAttribute("src", "/js/translate.js");
+script.setAttribute("crossorigin", "anonymous");
+// Get embed-id from the initial embed script and add it to script tags
+script.setAttribute(
+  "embed-id",
+  document.currentScript.getAttribute("embed-id")
+);
+
+document.getElementsByTagName("head")[0].appendChild(script);
+
+var script = document.createElement("script");
+
+script.setAttribute("type", "text/javascript");
+script.setAttribute("src", "/js/dist/widget.js");
+script.setAttribute("crossorigin", "anonymous");
+// Get embed-id from the initial embed script and add it to script tags
+script.setAttribute(
+  "embed-id",
+  document.currentScript.getAttribute("embed-id")
+);
+
+document.getElementsByTagName("head")[0].appendChild(script);
